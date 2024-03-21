37ccd89ea587eadb57fdcc973969d64b83ad7f44
unknown <parfenova-viktoria@yandex.ru>
2024-03-21 23:23:36 +0300

    add Lesson3

---
+++ Desktop/01_CSHARP/Lesson3/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вычисление дроби через функцию
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
