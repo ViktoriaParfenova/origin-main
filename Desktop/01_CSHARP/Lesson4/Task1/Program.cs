82495620ffa96bd206ae4641f7da561e8297486f
unknown <parfenova-viktoria@yandex.ru>
2024-03-31 19:29:47 +0300

    add Lesson4

---
+++ Desktop/01_CSHARP/Lesson4/Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Лекция двумерные массивы
+
+Создать двумерный массив с размерами 3*5,состоящий из целых чисел
+вывести его на экран
+
+
+int[,] matrix = new int[3, 5];//3строки и 5 столбцов,мы обозначили его память
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(1, 11);
+    }
+
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+*/
+int[,] CreateMatrix(int rowCount, int columCount)
+{
+    int[,] matrix = new int[rowCount, columCount];//3строки и 5 столбцов,мы обозначили его память
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
+
---
+++ Desktop/01_CSHARP/Lesson5/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Рекурсия
+/*
+void OpenMatryoshka(int size)
+{
+    Console.WriteLine($"Opening matryoshka of size {size}...");//Базовый случай - самая маленькая матрешка
+    if (size == 1)
+    {
+        Console.WriteLine("Reached the smallest matryoshka!");
+        return;
+    }
+    OpenMatryoshka(size - 1);
+}
+OpenMatryoshka(5);
+
+
+void OpenMatryoshka(int size)
+{
+if (size == 1)
+    {
+       Console.WriteLine("Smallest!");
+        return;
+    }
+OpenMatryoshka(size - 1);
+Console.WriteLine($"Opening matryoshka of size {size}...");
+}
+OpenMatryoshka(5);
+
+Вычислить факториал от натурального числа N
+*/
+
+int Fact(int n)
+{
+   if (n ==  1 || n == 0)
+   {
+    System.Console.WriteLine($"Stop: {n}");
+    return 1;
+   
+   }
+    return n * Fact(n - 1);
+    
+}
+System.Console.WriteLine(Fact(5));
